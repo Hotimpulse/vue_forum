cd4ba3c3fe775a786a5e309fb31974a24446b7bd
Hotimpulse <hotimpulse@hotmail.com>
2025-12-10 23:10:10 +0300

    feat: adding a route with id params

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
+import PageThreadShow from '@/components/PageThreadShow.vue'
 
 const routes = [
   {
     path: '/',
     component: PageHome,
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true,
   },
 ]
 
